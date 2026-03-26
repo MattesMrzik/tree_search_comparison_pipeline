dcdc0050b6acc3d6ba01937d2ea13cb53d475b10
Mattes Mrzik <mattes@mrzik.de>
2026-03-26 21:19:32 +0100

    use dendropy.calculate.treecompare to calc dists

--- scripts/calculate_distances.py
+++ scripts/calculate_distances.py
@@ -1,13 +1,11 @@
 import dendropy
-from dendropy.calculate import treecompare
 import sys
 import json
 import argparse
 
 def calculate_distances(tree1_path, tree2_path, output_path):
     try:
-        # PAML evolver output can be tricky. We use suppress_internal_node_taxa
-        # and suppress_leaf_node_taxa to avoid "duplicate taxon labels" errors,
+        # We use suppress_leaf_node_taxa to avoid "duplicate taxon labels" errors,
         # then we map them to a unified TaxonNamespace for comparison.
         tree1 = dendropy.Tree.get(
             path=tree1_path,
@@ -30,7 +28,7 @@ def calculate_distances(tree1_path, tree2_path, output_path):
     # Re-associate labels with a unified TaxonNamespace
     tns = dendropy.TaxonNamespace()
     
-    # Dendropy might not automatically map labels to Taxon objects if they were suppressed.
+    # Dendropy does not automatically map labels to Taxon objects if they were suppressed.
     # We manually populate the namespace and link the nodes.
     def reconcile_taxa(tree, namespace):
         for leaf in tree.leaf_node_iter():
@@ -45,18 +43,14 @@ def calculate_distances(tree1_path, tree2_path, output_path):
     tree1.taxon_namespace = tns
     tree2.taxon_namespace = tns
 
-    # Finalize for comparison
     tree1.is_rooted = False
     tree2.is_rooted = False
-    tree1.encode_bipartitions()
-    tree2.encode_bipartitions()
 
     # Robinson-Foulds distance (symmetric difference)
-    rf_distance = treecompare.symmetric_difference(tree1, tree2)
+    rf_distance = dendropy.calculate.treecompare.symmetric_difference(tree1, tree2)
     
     # Kuhner-Felsenstein distance (branch score distance)
-    # Note: treecompare.euclidean_distance in dendropy implements the Kuhner-Felsenstein distance
-    kf_distance = treecompare.euclidean_distance(tree1, tree2)
+    kf_distance = dendropy.calculate.treecompare.euclidean_distance(tree1, tree2)
 
     results = {
         "robinson_foulds": float(rf_distance),
